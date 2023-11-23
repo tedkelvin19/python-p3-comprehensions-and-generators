29585bdfe5a9712b74fe4b6b57a93eeebc3f15b9
Ted-Kelvin-Mutwiri <tedkelvin19@gmail.com>
2023-11-23 14:42:12 +0300

    Completed Lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,6 +2,9 @@
 
 def return_evens(num_list):
     pass
-
+    return [x for x in num_list if x % 2 == 0]
+return_evens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 def make_exclamation(sentence_list):
     pass
+    return [x + '!' for x in sentence_list]
+make_exclamation(["I like computers", "I require coffee", "Live long and prosper"])
